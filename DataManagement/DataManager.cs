22498ba6441d22d13bc52364175a2a2f1786229c
pevseev76 <pevseev@bmail.ru>
2016-05-08 17:39:12 +0400

    service interface IDataManager and the class DataManager imlementing it on empty implementation have been prepared

---
+++ DataManagement/DataManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace DataManagement
+{
+    public class DataManager : IDataManager
+    {
+        public void LoadLabels(IDictionary<string, string> labels) { }
+
+        public void LoadAdjancentNodes(string nodeId, IList<string> adjancentNodes) { }
+    }
+}
---
+++ DataManagement/IDataManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace DataManagement
+{
+    [ServiceContract]
+    public interface IDataManager
+    {
+        [OperationContract]
+        void LoadLabels(IDictionary<string, string> labels);
+
+        [OperationContract]
+        void LoadAdjancentNodes(string nodeId, IList<string> adjancentNodes);
+    }
+}
