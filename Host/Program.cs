3c77c09d0748232182bc474bdd1e197c93c110e2
pevseev76 <pevseev@bmail.ru>
2016-05-09 16:52:51 +0400

    the host to start the DataManager service is ready

--- Host/Program.cs
+++ Host/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using DataManagement;
 
 namespace Host
 {
@@ -10,6 +13,29 @@ namespace Host
     {
         static void Main(string[] args)
         {
+            var baseAddress = new Uri("http://localhost:8000/DataManagement/");
+            var selfHost = new ServiceHost(typeof(DataManager), baseAddress);
+
+            try
+            {
+                selfHost.AddServiceEndpoint(typeof(IDataManager), new WSHttpBinding(), "DataManager");
+
+                var smb = new ServiceMetadataBehavior() { HttpGetEnabled = true };
+                selfHost.Description.Behaviors.Add(smb);
+
+                selfHost.Open();
+
+                Console.WriteLine("The DataManager service has been started. Please <ENTER> to terminate it.");
+                Console.WriteLine();
+                Console.ReadLine();
+
+                selfHost.Close();
+            }
+            catch (CommunicationException e)
+            {
+                Console.WriteLine("An exception occured {0} ", e.Message);
+                selfHost.Abort();
+            }
         }
     }
 }
