b3749cab8fd0d066d814a4404aab76f2cb5f8136
pevseev76 <pevseev@bmail.ru>
2016-05-10 20:01:58 +0400

    tesing for the method DataProvider::GetAdjacentNodes is ready

--- DataProviderTest/DataProviderTest.cs
+++ DataProviderTest/DataProviderTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
 using System.Data.SqlClient;
@@ -58,6 +59,37 @@ namespace DataProviderTest
 
                     Assert.AreEqual(labels[key], actualLabels[actualKey]);
                 }
+
+                var adjacentNodes = new Dictionary<string, List<string>>();
+
+                adjacentNodes["first"] = (new string[] { "second", "third", "fourth" }).ToList();
+                adjacentNodes["second"] = (new string[] { "first", "third", "fourth", "sixth" }).ToList();
+                adjacentNodes["third"] = (new string[] { "seventh", "third", "first" }).ToList();
+                adjacentNodes["fourth"] = (new string[] { "seventh", "third", "fourth" }).ToList();
+                adjacentNodes["eleventh"] = (new string[] { "second", "third", "fourth" }).ToList();
+                adjacentNodes["seventh"] = (new string[] { "second", "fourth", "sixth", "tenth" }).ToList();
+
+                foreach (var key in adjacentNodes.Keys)
+                    for (int i = 0; i < adjacentNodes[key].Count; i++)
+                        InsertAdjancentNode(key, adjacentNodes[key][i]);
+
+                var actualAdjacentNodes = provider.GetAdjacentNodes();
+
+                foreach (var key in adjacentNodes.Keys)
+                {
+                    string actualKey = key.PadRight(idLength);
+
+                    if (!actualAdjacentNodes.Keys.Contains(actualKey))
+                        Assert.Fail();
+
+                    for (int i = 0; i < adjacentNodes[key].Count; i++)
+                    {
+                        string adjancentNode = adjacentNodes[key][i].PadRight(idLength);
+
+                        if (!actualAdjacentNodes[actualKey].Contains(adjancentNode))
+                            Assert.Fail();
+                    }
+                }
             }
             finally
             {
@@ -85,6 +117,24 @@ namespace DataProviderTest
                 command.CommandType = System.Data.CommandType.Text;
                 command.Parameters.AddWithValue("@id", id);
                 command.Parameters.AddWithValue("@label", label);
+                command.ExecuteNonQuery();
+            }
+        }
+
+        private void InsertAdjancentNode(string id, string adjancent)
+        {
+            using (var command = new SqlCommand("StoredProcedure1", connection))
+            {
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+
+                var nodeId = new SqlParameter("@parameter1", System.Data.SqlDbType.Char, idLength);
+                nodeId.Value = id;
+                command.Parameters.Add(nodeId);
+
+                var adjancentId = new SqlParameter("@parameter2", System.Data.SqlDbType.Char, idLength);
+                adjancentId.Value = adjancent;
+                command.Parameters.Add(adjancentId);
+
                 command.ExecuteNonQuery();
             }
         }
