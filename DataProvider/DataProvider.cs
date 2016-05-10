0aa5a49701d3771e0d835f71d591c5e5e2c9b39a
pevseev76 <pevseev@bmail.ru>
2016-05-10 18:19:59 +0400

    the service DataProvider has been added to the solution

---
+++ DataProvider/DataProvider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace DataProvider
+{
+    public class DataProvider : IDataProvider
+    {
+        public IDictionary<string, string> GetLabels()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<string> GetAdjancentNodes(string nodeId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DataProvider/IDataProvider.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace DataProvider
+{
+    [ServiceContract]
+    public interface IDataProvider
+    {
+        [OperationContract]
+        IDictionary<string, string> GetLabels();
+
+        [OperationContract]
+        IList<string> GetAdjancentNodes(string nodeId);
+    }
+}
