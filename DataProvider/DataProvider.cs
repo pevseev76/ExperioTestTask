04e59244f110f9a030ed4f866f9e7a4c11c17624
pevseev76 <pevseev@bmail.ru>
2016-05-10 19:36:33 +0400

    the method DataProvider::GetLabels is ready

--- DataProvider/DataProvider.cs
+++ DataProvider/DataProvider.cs
@@ -4,19 +4,68 @@ using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
+using System.Data.SqlClient;
 
 namespace DataProvider
 {
-    public class DataProvider : IDataProvider
+    public class DataProvider : IDataProvider, IDisposable
     {
+        private const int idLength = 10;
+
+        #region Connection string parameters
+
+        private const string dataSource = "Data Source=.\\SQLEXPRESS;";
+        private const string initialCatalog = "Initial Catalog = NodesBD;";
+        private const string integratedSecurity = "Integrated Security=true;";
+        private const string pooling = "Pooling=false";
+
+        #endregion
+
+        private readonly SqlConnection connection;
+
+        public DataProvider()
+        {
+            string connectionString = dataSource + initialCatalog + integratedSecurity + pooling;
+            connection = new SqlConnection(connectionString);
+        }
+        
         public IDictionary<string, string> GetLabels()
         {
-            throw new NotImplementedException();
+            var result = new Dictionary<string, string>();
+
+            try
+            {
+                SqlDataReader dataReader;
+                
+                connection.Open();
+
+                string selectAllLabelsQuery = "SELECT IdenticalOfNode, Label FROM Labels";
+
+                using (var command = new SqlCommand(selectAllLabelsQuery, connection))
+                {
+                    command.CommandType = System.Data.CommandType.Text;
+                    dataReader = command.ExecuteReader();
+                }
+
+                while (dataReader.Read())
+                    result[dataReader["IdenticalOfNode"].ToString()] = dataReader["Label"].ToString();
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return result;
         }
 
         public IList<string> GetAdjancentNodes(string nodeId)
         {
             throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            connection.Dispose();
         }
     }
 }
