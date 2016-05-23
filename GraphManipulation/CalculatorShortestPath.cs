24bc2dbd95ec7c474f4c5d63cfac5d092620dea7
pevseev76 <pevseev@bmail.ru>
2016-05-23 22:10:50 +0400

    The project GraphManipulation is added

---
+++ GraphManipulation/CalculatorShortestPath.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace GraphManipulation
+{
+    public class CalculatorShortestPath : ICalculatorShortestPath
+    {
+        public IList<string> CalculateShortestPath(string firstID, string secondID)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GraphManipulation/ICalculatorShortestPath.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace GraphManipulation
+{
+    [ServiceContract]
+    public interface ICalculatorShortestPath
+    {
+        [OperationContract]
+        IList<string> CalculateShortestPath(string firstID, string secondID);
+    }
+}
