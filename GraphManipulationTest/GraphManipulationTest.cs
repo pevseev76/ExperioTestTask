ce714176048a79ad73476fc603753c258054a415
pevseev76 <pevseev@bmail.ru>
2016-05-24 22:08:56 +0400

    The project GraphManipulatorTest is added to test the project GraphManipulation

---
+++ GraphManipulationTest/GraphManipulationTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GraphManipulatorTest
+{
+    [TestClass]
+    public class GraphManipulationTest
+    {
+        [TestMethod]
+        public void TestIsolatedPoints()
+        {
+        }
+    }
+}
