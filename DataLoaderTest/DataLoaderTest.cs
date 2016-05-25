a8778152a8d35d508998727316d2d00647ab76ee
pevseev76 <pevseev@bmail.ru>
2016-05-25 22:21:12 +0400

    The project DataLoaderTest is added to test DataLoader

---
+++ DataLoaderTest/DataLoaderTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataLoaderTest
+{
+    [TestClass]
+    public class DataLoaderTest
+    {
+        [TestMethod]
+        public void DataLoadTest()
+        {
+        }
+    }
+}
