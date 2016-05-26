cfce15f809385ca276291e66572bf55c33a1df5a
pevseev76 <pevseev@bmail.ru>
2016-05-26 23:22:07 +0400

    The class NodeViewModel is added

---
+++ ThinClient/NodeViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace ThinClient
+{
+    public class NodeViewModel
+    {
+        string ID { get; set; }
+        string Label { get; set; }
+        FrameworkElement Element { get; set; }
+        Color Background { get; set; }
+        Color Border { get; set; }
+    }
+}
